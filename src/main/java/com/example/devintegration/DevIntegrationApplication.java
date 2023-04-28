9126f4a0b793ef81691652ea42d77abd1f7b2f3d
SOUROURI-EXTERNAL Safae <S0130575@gemalto.com>
2023-04-28 12:03:16 +0200

    first commit

---
+++ src/main/java/com/example/devintegration/DevIntegrationApplication.java
@@ -0,0 +1,16 @@
+package com.example.devintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevIntegrationApplication.class, args);
+		System.out.println("Devops ");
+		System.out.println("Integration ");
+
+	}
+
+}
---
+++ src/test/java/com/example/devintegration/DevIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
