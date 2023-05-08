ed92fe40016abe6948349ac1bdde2265e1993fbb
SOUROURI-EXTERNAL Safae <S0130575@gemalto.com>
2023-05-08 10:17:25 +0200

    third Commit

--- src/main/java/com/example/devintegration/DevIntegrationApplication.java
+++ src/main/java/com/example/devintegration/DevIntegrationApplication.java
@@ -8,8 +8,10 @@ public class DevIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevIntegrationApplication.class, args);
-		System.out.println("Devops ");
-		System.out.println("Integration ");
+		System.out.println("Pull -> git ");
+		System.out.println("Build -> maven");
+		System.out.println(" Test -> junit ");
+
 
 	}
 
