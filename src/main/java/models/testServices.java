f740189ea8faa79e0e186d4214a1b8b47b8db787
s0130575 <Sourouri01@gmail.com>
2023-05-10 13:28:20 +0200

    changes

--- src/main/java/models/testServices.java
+++ src/main/java/models/testServices.java
@@ -4,10 +4,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class testServices {
-    public int add(int a , int b ){
-        int sum = a ;
 
-        return  sum ;
-
-    }
 }
